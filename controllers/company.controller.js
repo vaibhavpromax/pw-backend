35c2dc179dbc93dad4730783e4b72cb694ecfcc5
vaibhavpromax <singhvaibhav557hr@gmail.com>
2023-06-26 18:16:39 +0530

    fix

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -95,54 +95,90 @@ const getListings = async (req, res) => {
 
   let options = { where: {} };
 
+  // pagination
+  let page = 0;
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+    page = pageAsNumber;
+  }
+
+  let size = 10;
+  if (
+    !Number.isNaN(sizeAsNumber) &&
+    !(sizeAsNumber > 10) &&
+    !(sizeAsNumber < 1)
+  ) {
+    size = sizeAsNumber;
+  }
+
+  options.limit = size;
+  options.offset = page * size;
+
   if (sort === "new") options.order = [["expiry_date", "DESC"]];
-  if (category) options.where.company_status = { [Op.in]: category };
-  if (locations) options.where.location = { [Op.in]: locations };
+  if (category) {
+    delete options.limit;
+    delete options.offset;
+    options.where.company_status = { [Op.in]: category };
+  }
+  if (locations) {
+    delete options.limit;
+    delete options.offset;
+    options.where.location = { [Op.in]: locations };
+  }
   if (stipend_end && stipend_start) {
     if (stipend_end == 40001) {
-      // [Op.or]: {
-      //   options.where.stipend_min = { [Op.gte]: stipend_start };
-      //   options.where.stipend_max = { [Op.lte]: stipend_end };
-      //
-      // options.where.stipend_min = {
-      //   [Op.gte]: stipend_start,
-      // };
       options.where.stipend_max = {
-        [Op.or]: {
-          [Op.gte]: stipend_end,
-        },
+        [Op.gte]: stipend_end,
       };
     } else {
       options.where.stipend_min = { [Op.gte]: stipend_start };
       options.where.stipend_max = { [Op.lte]: stipend_end };
     }
+    delete options.limit;
+    delete options.offset;
   }
-  if (applicants_start && applicants_end) {
-    if (applicants_end == 251)
+
+  if (applicants_start >= 0 && applicants_end >= 0) {
+    if (applicants_end == 251) {
       options.where.total_applicants = {
         [Op.or]: {
           // [Op.between]: [applicants_start, applicants_end],
           [Op.gte]: applicants_start,
         },
       };
-    else
+    } else
       options.where.total_applicants = {
         [Op.between]: [applicants_start, applicants_end],
       };
+    delete options.limit;
+    delete options.offset;
   }
   if (duration_start && duration_end) {
-    if (duration_end == 7)
+    if (duration_end == 6) {
       options.where.duration = {
         [Op.gte]: duration_start,
       };
-    else
+    } else
       options.where.duration = {
-        $between: [duration_start, duration_end],
+        [Op.between]: [parseInt(duration_start), parseInt(duration_end)],
       };
+    delete options.limit;
+    delete options.offset;
   }
-  if (timing) options.where.timing = timing;
-  if (type) options.where.job_type = type;
-  if (name) options.where.name = name;
+  if (timing) {
+    delete options.limit;
+    delete options.offset;
+    options.where.timing = timing;
+  }
+  if (type) {
+    delete options.limit;
+    delete options.offset;
+    options.where.job_type = type;
+  }
+  if (name) {
+    delete options.limit;
+    delete options.offset;
+    options.where.name = name;
+  }
   // if (skill) {
   //   // options.where.skills = {
   //   //   [Op.eq]: [
@@ -161,24 +197,6 @@ const getListings = async (req, res) => {
   //   );
   // }
 
-  // pagination
-  let page = 0;
-  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
-    page = pageAsNumber;
-  }
-
-  let size = 10;
-  if (
-    !Number.isNaN(sizeAsNumber) &&
-    !(sizeAsNumber > 10) &&
-    !(sizeAsNumber < 1)
-  ) {
-    size = sizeAsNumber;
-  }
-
-  options.limit = size;
-  options.offset = page * size;
-  console.log(options);
   try {
     let listings = {};
     if (skill) {
@@ -191,7 +209,8 @@ const getListings = async (req, res) => {
         ),
       });
     } else listings = await Company.findAndCountAll(options);
-    return successResponse(res, "Fetched count successfully", listings);
+    console.log(listings);
+    return successResponse(res, "Fetched data successfully", listings);
   } catch (error) {
     logger.error("Error while fetchign", error);
     return serverErrorResponse(res, "Error while fetching");
